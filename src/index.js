3095de37139a3fd4102a4c446e709048405e7bb9
Rob Cameron <rob.cameron@fastmail.com>
2020-04-30 14:23:21 -0700

    Also replace provider in migration snapshots of schema.prisma

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
-const fs = require("fs");
-const PROVIDER_REGEX = /^\s+provider\s+=\s+["']sqlite["']/m;
+const fs = require('fs')
+const path = require('path')
+
+const glob = require('glob')
+const PROVIDER_REGEX = /^\s*provider\s+=\s+["']sqlite["']/m
+
+const replaceProvider = (path, varName) => {
+  const schema = fs.readFileSync(path).toString()
+  const newProvider = `provider = "${process.env[varName] || 'postgresql'}"`
+
+  if (schema.match(PROVIDER_REGEX)) {
+    const newSchema = schema.replace(PROVIDER_REGEX, newProvider)
+
+    fs.writeFileSync(path, newSchema)
+    console.log(`  Replaced provider with \`${newProvider}\` in ${path}`)
+  } else {
+    console.log(`  Skipping, provider is not "sqlite" in ${path}`)
+  }
+}
 
 module.exports = {
   onPreBuild: ({ inputs }) => {
-    const schema = fs.readFileSync(inputs.path).toString();
-    const newProvider = `provider = "${process.env[inputs.varName] || "postgresql"}"`;
+    console.log('Replacing provider in schema.prisma...')
+    replaceProvider(inputs.path, inputs.varName)
 
-    if (schema.match(PROVIDER_REGEX)) {
-      const newSchema = schema.replace(PROVIDER_REGEX, newProvider);
+    console.log('Replacing providers in migrations...')
+    const pathParts = inputs.path.split('/')
+    pathParts.splice(pathParts.length - 1, 0, 'migrations', '**')
+    const migrationPath = path.join(...pathParts)
 
-      fs.writeFileSync(inputs.path, newSchema);
-      console.log(`  Replaced provider with \`${newProvider}\``);
-    } else {
-      console.log('  Skipping, provider is not "sqlite"');
-    }
-  }
-};
+    glob.sync(migrationPath).forEach((file) => {
+      replaceProvider(file, inputs.varName)
+    })
+  },
+}
